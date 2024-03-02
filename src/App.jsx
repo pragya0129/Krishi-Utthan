ee2f249b826dafe1c46453bec1ebdb3f9ac12245
Pragya Gaur <gaur273pragya@gmail.com>
2024-03-02 12:40:50 +0530

    edit top section

--- src/App.jsx
+++ src/App.jsx
@@ -25,12 +25,35 @@ function App() {
             <a onClick={() => scrollToSection("buy")}>Buy</a>
           </li>
           <li>
+            <a onClick={() => scrollToSection("recommendation")}>
+              Recommendation
+            </a>
+          </li>
+          <li>
+            <a onClick={() => scrollToSection("weather")}>Weather</a>
+          </li>
+          <li>
             <a onClick={() => scrollToSection("faq")}>FAQ</a>
           </li>
           <li>
-            <a onClick={() => scrollToSection("contact")}>Contact</a>
+            <a onClick={() => scrollToSection("about")}>About</a>
           </li>
         </ul>
+      </div>
+
+      <div className="top" id="home">
+        <br className="showhide"></br>
+        <h1>
+          <span className="heading">Krishi Utthan</span>
+        </h1>
+        <br></br>
+        <div className="text">
+          description
+          <br></br>
+          <br></br>
+        </div>
+
+        <p className="animtext">animated text</p>
       </div>
     </div>
   );
