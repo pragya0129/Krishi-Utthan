059a626e740313dfe02d1ee09f7a834c9de52b3f
Pragya Gaur <gaur273pragya@gmail.com>
2024-03-02 12:10:23 +0530

    Added Nav Bar

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ Krishi Udyog
 Packages:
 
 npm i react-router-dom react-icons react-type-animation
+
+Tailwind : npm install -D tailwindcss
+npx tailwindcss init
--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const scrollToSection = (id) => {
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <div className="navigation">
+        <ul className="nav-links">
+          <li>
+            <img src=""></img>
+          </li>
+          <li>
+            <a onClick={() => scrollToSection("home")}>Home</a>
+          </li>
+          <li>
+            <a onClick={() => scrollToSection("buy")}>Buy</a>
+          </li>
+          <li>
+            <a onClick={() => scrollToSection("faq")}>FAQ</a>
+          </li>
+          <li>
+            <a onClick={() => scrollToSection("contact")}>Contact</a>
+          </li>
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
